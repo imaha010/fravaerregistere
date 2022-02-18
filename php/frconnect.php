a198841f3910c9097189f1d3c9e122d9c06cfe54
imaha010 <91658661+imaha010@users.noreply.github.com>
2022-02-18 09:11:28 +0100

    dkkdøldlj
    eijefef

--- php/frconnect.php
+++ php/frconnect.php
@@ -1,1 +1,30 @@
-/* moren din */
+<?php
+
+$bruker = $_POST['user'];
+$pass = $_POST['password'];
+
+
+  //Opprette kobling
+  $kobling = new mysqli('localhost', 'root', '', 'fravaerregister');
+		
+  //Sjekk om kobling virker
+  if($kobling->connect_error) {
+	  die("Noe gikk galt: " . $kobling->connect_error);
+  }
+
+
+	  //Angi UTF-8 som tegnsett
+	  $kobling->set_charset("utf8");	
+  
+	  $sql = "INSERT INTO inho (bruker, pass) VALUES ('$bruker', '$pass')";
+  
+	  if($kobling->query($sql)) {
+		  echo "Spørringen $sql ble gjennomført. ";
+	  }else{
+		  echo "Noe gikk galt med spørringen $sql ($kobling->error).";
+	  }
+
+    $
+
+ 
+?>
